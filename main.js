b4bb110742c33e9d73f254fa5e8f3e96d6c81754
Dmitry Bondarenko <65862113+Logikmars@users.noreply.github.com>
2024-10-03 17:35:52 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -4,26 +4,27 @@ let fBtn = document.getElementsByClassName("f-btn")[0];
 let sBtn = document.getElementsByClassName("s-btn")[0];
 
 const form = document.getElementsByClassName("test-form")[0];
-
+const plane = document.getElementById("plane");
+const imageplane = document.getElementById("imageplane");
 // Скрыть главный блок и показать форму
 fBtn.addEventListener("click", () => {
-    document.getElementsByClassName("Main")[0].style.display = "none";
-    form.style.display = "flex";
+    plane.style.transition = "2s cubic-bezier(0.68, -0.55, 0.27, 1.55)";
+    plane.style.transform = "translateY(-100dvh)";    
 });
 
 // Отправка данных
-sBtn.addEventListener("click", (event) => {
-    event.preventDefault(); // Предотвращает стандартное поведение кнопки
+// sBtn.addEventListener("click", (event) => {
+//     event.preventDefault(); // Предотвращает стандартное поведение кнопки
 
-    let title = document.getElementsByClassName("title-inp")[0].value;
-    let description = document.getElementsByClassName("desc-inp")[0].value;
-    let text = document.getElementsByClassName("text-inp")[0].value;
-    document.getElementsByClassName("Main")[0].style.display = "flex";
-    let data = {
-        title: title,
-        desc: description,
-        text: text
-    };
+//     let title = document.getElementsByClassName("title-inp")[0].value;
+//     let description = document.getElementsByClassName("desc-inp")[0].value;
+//     let text = document.getElementsByClassName("text-inp")[0].value;
+//     document.getElementsByClassName("Main")[0].style.display = "flex";
+//     let data = {
+//         title: title,
+//         desc: description,
+//         text: text
+//     };
 
-    tg.sendData(JSON.stringify(data));
-});
+//     tg.sendData(JSON.stringify(data));
+// });
