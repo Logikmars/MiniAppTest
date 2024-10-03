5d03168c5fffe624118827f1d0f8c454fdc94e5b
Dmitry Bondarenko <65862113+Logikmars@users.noreply.github.com>
2024-10-03 18:14:28 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -6,10 +6,26 @@ let sBtn = document.getElementsByClassName("s-btn")[0];
 const form = document.getElementsByClassName("test-form")[0];
 const plane = document.getElementById("plane");
 const imageplane = document.getElementById("imageplane");
+const title = document.getElementById("title");
+// Блок с квизами
+const quiz = document.getElementById("quiz");
 // Скрыть главный блок и показать форму
 fBtn.addEventListener("click", () => {
     plane.style.transition = "2s cubic-bezier(0.68, -0.55, 0.27, 1.55)";
-    plane.style.transform = "translateY(-100dvh)";    
+    plane.style.transform = "translateY(-100dvh)"; 
+    setTimeout(() => {
+        fBtn.style.transition = "4s ease";
+        fBtn.style.transform = "translateY(150dvh)";
+        title.style.transition = "2.5s ease";
+        title.style.transform = "translateY(150dvh)";
+        setTimeout(() => {
+            imageplane.style.display = "none";
+            fBtn.style.display = "none";
+            title.style.display = "none";
+        }, 1500);
+        // Тут нужно что бы спускался экран с квизами
+        quiz.style.transform = "translateY(100dvh)";
+    }, 500);   
 });
 
 // Отправка данных
